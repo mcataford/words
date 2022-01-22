bf80bc24d86ddf22e8f6c411a27e65b65fc78fa7
Marc Cataford <mcat@riseup.net>
2022-01-22 16:53:54 -0500

    feat: add common words dict

--- src/build.py
+++ src/build.py
@@ -4,17 +4,18 @@ in the adjacent sourcelist (sources.txt).
 """
 
 import urllib.request
-import sys
 import pathlib
 import re
 import logging
 
 SOURCES_PATH = pathlib.Path(pathlib.Path.cwd(), "sources.txt")
-DICTIONARY_PATH = pathlib.Path(pathlib.Path.cwd(), "dictionary.txt")
+FULL_DICTIONARY_PATH = pathlib.Path(pathlib.Path.cwd(), "dictionary_full.txt")
+COMMON_DICTIONARY_PATH = pathlib.Path(pathlib.Path.cwd(), "dictionary_common.txt")
+
+COMMON_FREQUENCY_THRESHHOLD = 5
 
 logger = logging.getLogger('words-cli')
 logging.basicConfig(level=logging.INFO)
-
 
 def build_from_sources():
     """
@@ -27,9 +28,9 @@ def build_from_sources():
     with open(SOURCES_PATH, "r") as sources:
         sourcelist = [source.strip() for source in sources.read().split('\n') if source]
 
-    words = set()
-
     logger.info("Fetching from %d sources" % len(sourcelist))
+
+    words_by_count = {}
 
     for source in sourcelist:
         logger.info("Fetching from %s..." % source)
@@ -44,44 +45,28 @@ def build_from_sources():
             response_text = str(raw_response)
 
         for word_candidate in re.split(r"\s", response_text):
-            stripped = word_candidate.strip().lower()
+            normalized = word_candidate.strip().lower()
 
-            if re.match(r"^[a-z]{3,}$", stripped):
-                words.add(stripped)
+            if re.match(r"^[a-z]{3,}$", normalized):
+                words_by_count[normalized] = words_by_count.get(normalized, 0) + 1
 
-    ordered = sorted(list(words))
+    full_dictionary = set()
+    common_dictionary = set()
 
-    with open(DICTIONARY_PATH, "w") as dictionary:
-        dictionary.write('\n'.join(ordered))
+    for word, count in words_by_count.items():
+        if count >= COMMON_FREQUENCY_THRESHHOLD:
+            common_dictionary.add(word)
 
-    logger.info("Dictionary length: %d words" % len(ordered))
+        full_dictionary.add(word)
 
-def add_to_sources(url: str):
-    """
-    Adds a new resource to the sourcelist. The entries are assumed to be
-    valid URLs pointing to plaintext files.
-    """
+    with open(FULL_DICTIONARY_PATH, "w") as dictionary:
+        dictionary.write('\n'.join(sorted(list(full_dictionary))))
+    
+    with open(COMMON_DICTIONARY_PATH, "w") as dictionary:
+        dictionary.write('\n'.join(sorted(list(common_dictionary))))
 
-    with open(SOURCES_PATH, "a") as sources:
-        sources.write(url + "\n")
-
+    logger.info("Full dictionary length: %d words" % len(full_dictionary))
+    logger.info("Common words dictionary length: %d words" % len(common_dictionary))
+    
 if __name__ == "__main__":
-    """
-    Commands:
-        add <url>
-            Adds the given URL to the sourcelist.
-        build
-            Builds a dictionary of unique words from the sourcelist resources.
-    """
-    args = sys.argv[1:]
-
-    if len(args) < 1:
-        raise Exception("Missing options")
-
-    action = args[0]
-
-    if action == "add" and len(args) == 2:
-        add_to_sources(args[1])
-
-    elif action == "build":
-        build_from_sources()
+    build_from_sources()
